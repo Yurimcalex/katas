1dda8ed932243c4485d6eddcc110ffec8128107b
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-28 15:31:18 +0200

    Update puzzle cell class

--- src/towers/puzzleCell.js
+++ src/towers/puzzleCell.js
@@ -1,25 +1,34 @@
 export default class PuzzleCell {
-	constructor(leftCount, rightCount, topCount, bottomCount, r, c, initialVariants) {
-		this.name = r + '' + c;
-		this.r = r;
-		this.c = c;
-		this.rowKey = String(leftCount) + String(rightCount);
-		this.colKey = String(topCount) + String(bottomCount);
-		this.variants = initialVariants;
+	constructor(rowLeftTops, rowRightTops, colLeftTops, colRightTops, rowIndex, colIndex, lineSize) {
+		this.name = `${rowIndex}${colIndex}`;
+		this.r = rowIndex;
+		this.c = colIndex;
+		this.rowKey = `${rowLeftTops}${rowRightTops}`;
+		this.colKey = `${colLeftTops}${colRightTops}`;
+		this.possibleValues = this.getInitialValues(lineSize);
 		this.result = null;
 	}
 
-	check(variants) {
-		const result = variants.filter(v => !!v);
-		if (result.length === 1) return result[0];
-		return 0;
+	getInitialValues(n) {
+		const result = [];
+		for (let i = 0; i < n; i += 1) {
+			result.push(i + 1);
+		}
+		return result;
 	}
 
-	calc(variants) {
-		if (!variants.length) return;
-		const newVariants = this.variants.map(v => variants.includes(v) ? v : 0);
-		const result = this.check(newVariants);
-		if (result) this.result = result;
-		this.variants = newVariants;
+	excludeValues(updatedValues) {
+		this.possibleValues = this.possibleValues.map(v => updatedValues.includes(v) ? v : 0);
+	}
+
+	calculateResult() {
+		const values = this.possibleValues.filter(v => !!v);
+		if (values.length === 1) this.result = values[0];
+	}
+
+	calc(updatedValues) {
+		if (!updatedValues.length) return;
+		this.excludeValues(updatedValues);
+		this.calculateResult();		
 	}
 }
