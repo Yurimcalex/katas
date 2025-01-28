9a23597f82ef405bc9132004026fade907bd4864
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-28 15:31:47 +0200

    Update lookup table class

--- src/towers/lookupTable.js
+++ src/towers/lookupTable.js
@@ -1,26 +1,17 @@
+import { generateArrays, calcVisibleTops } from './utils.js';
+
+
 export default class LookupTable {
 	constructor(n) {
-		this.make(n);
+		this.makeList(n);
 	}
-
-	calcVisibleTops(arr) {
-		let count = 0;
-		let max = 0;
-		for (let t of arr) {
-			if (t - max > 0) {
-				count += 1;
-				max = t;
-			}
-		}
-		return count;
-	}
-
-	make(n) {
+	
+	makeList(n) {
 		const list = {};
 		const variants = generateArrays(1, n);
 		variants.forEach(arr => {
-			const leftTops = this.calcVisibleTops(arr);
-			const rightTops = this.calcVisibleTops([...arr].reverse());
+			const leftTops = calcVisibleTops(arr);
+			const rightTops = calcVisibleTops([...arr].reverse());
 			const key = [leftTops, rightTops].join('');
 			if (list[key]) {
 				list[key].push(arr);
@@ -31,7 +22,7 @@ export default class LookupTable {
 		this.list = list;
 	}
 
-	getItems(key) {
+	getListItemsByKey(key) {
 		const items = [];
 		for (let item in this.list) {
 			if ( key === '00' || 
@@ -59,44 +50,10 @@ export default class LookupTable {
 		return result;
 	}
 
-	findVariants(key, ind, template) {
-		const items = this.getItems(key);
+	findPossibleValues(key, ind, template) {
+		const items = this.getListItemsByKey(key);
 		const filteredItems = this.filterItems(items, template);
-		const variants = filteredItems.map((item) => item[ind]);
-		return Array.from(new Set(variants));
+		const values = filteredItems.map((item) => item[ind]);
+		return Array.from(new Set(values));
 	}
 }
-
-
-// generate all possible arrays of combinations of n, m numbers, each of length m
-function generateArrays(n, m) {
-	const list = [];
-	const startArr = [];
-	for (let i = n; i <= m; i += 1) startArr.push(i);
-	function generate(item, arr) {
-		if (!arr.length) list.push(item);
-		for (let i = 0; i < arr.length; i += 1) {
-			const nextItem = [...item];
-			const nextArr = [...arr];
-			nextItem.push(arr[i]);
-			nextArr.splice(i, 1);
-			generate(nextItem, nextArr);
-		}
-	}
-	generate([], startArr);
-	return list;
-}
-
-
-// document.body.innerHTML = `<pre>${(function () {
-// 	let str = '';
-// 	const list = (new LookupTable(1, 4)).list;
-// 	console.log(list);
-// 	for (let count in list) {
-// 		str += `${count}:\n`
-// 		str += '-------\n';
-// 		str += list[count].join('\n');
-// 		str += '\n\n';
-// 	}
-// 	return str;
-// })()}</pre>`;
