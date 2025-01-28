264c4775440a9ff91b120022b54ca458a05e4e3c
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-28 23:54:46 +0200

    Add more test cases

--- src/towers/index.js
+++ src/towers/index.js
@@ -57,7 +57,9 @@ class Puzzle {
 	}
 
 	solve() {
+		let counter = 0;
 		while (this.cells.length) {
+			counter++;
 			for (let i = 0; i < this.cells.length; i += 1) {
 				let cell = this.cells[i];
 				const { rowKey, colKey, r, c } = cell;
@@ -82,15 +84,45 @@ class Puzzle {
 					}
 				}
 			}
+			if (counter >= 100) {
+				console.log('COUNTER!');
+				return;
+			}
 		}
 	}
 }
 
+
+const data = [
+	[ 2, 2, 1, 3, 2, 2, 3, 1, 1, 2, 2, 3, 3, 2, 1, 3 ],
+	[ 1, 2, 4, 2, 2, 1, 3, 2, 3, 1, 2, 3, 3, 2, 2, 1 ],
+	[ 3, 2, 2, 1, 1, 2, 4, 2, 2, 1, 3, 2, 3, 1, 2, 3 ],
+	[ 0, 0, 1, 2, 0, 2, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0 ],
+	[ 3, 2, 2, 1, 1, 2, 4, 2, 2, 1, 3, 2, 3, 1, 2, 3 ],
+	[ 2, 1, 3, 2, 3, 1, 2, 3, 3, 2, 2, 1, 1, 2, 4, 2 ],
+	[ 2, 2, 3, 1, 1, 2, 2, 3, 3, 2, 1, 3, 2, 2, 1, 3 ],
+	[ 3, 1, 2, 3, 3, 2, 2, 1, 1, 2, 4, 2, 2, 1, 3, 2 ],
+	[ 1, 2, 2, 3, 3, 2, 1, 3, 2, 2, 1, 3, 2, 2, 3, 1 ],
+	[ 3, 1, 2, 3, 3, 2, 2, 1, 1, 2, 4, 2, 2, 1, 3, 2 ],
+	[ 0, 2, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0, 0, 0, 1, 2 ]
+];
+
+
 console.time('start');
-for (let i = 0; i < 1000; i += 1) {
+//for (let i = 0; i < 1000; i += 1) {
 	const clues = [0, 0, 1, 2, 0, 2, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0];
 	const puzzle = new Puzzle(4, clues);
 	puzzle.solve();
-}
+//}
 console.timeEnd('start');
 //puzzle.result.table.forEach(line => console.log(line));
+
+console.time('data');
+for (let d of data) {
+	const puzzle = new Puzzle(4, d);
+	puzzle.solve();
+	puzzle.result.table.forEach(line => console.log(line));
+	console.log(JSON.stringify(d));
+	console.log('-------------------------------------------------');
+}
+console.timeEnd('data');
