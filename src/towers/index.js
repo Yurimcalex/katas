37934d26a8167266583763525bd6ea1e3b85e867
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-28 15:32:08 +0200

    Update puzzle class

--- src/towers/index.js
+++ src/towers/index.js
@@ -1,101 +1,85 @@
 import LookupTable from './lookupTable.js';
 import PuzzleResult from './puzzleResult.js';
 import PuzzleCell from './puzzleCell.js';
+import { sumArraysValues } from './utils.js';
 
 class Puzzle {
-	constructor(n, clues) {
-  	this.result = new PuzzleResult(n);
-  	this.lookupTable = new LookupTable(n);
+	constructor(size, clues) {
+  	this.result = new PuzzleResult(size);
+  	this.lookupTable = new LookupTable(size);
   	this.cells = [];
-  	this.init(n, clues);
+  	this.init(size, clues);
 	}
 
-	getInitialCellVariants(n) {
-		const result = [];
-		for (let i = 0; i < n; i += 1) {
-			result.push(i + 1);
-		}
-		return result;
-	}
-
-	init(n, clues) {
+	init(size, clues) {
 		let r = 0, c = 0;
 		for (let i = 0; i < clues.length; i += 1) {
 			const cell = new PuzzleCell(
-				clues[n * n - 1 - r],
-				clues[n + r],
+				clues[size * size - 1 - r],
+				clues[size + r],
 				clues[c],
-				clues[n * (n - 1) - 1 - c],
+				clues[size * (size - 1) - 1 - c],
 				r,
 				c,
-				this.getInitialCellVariants(n)
+				size
 			);
 			this.cells.push(cell);
 			c += 1;
-			if (c === n) {
+			if (c === size) {
 				c = 0;
 				r += 1;
 			}
 		}
 	}
 
-	excludeVariant(cell) {
-		const cells = this.cells.filter(c => c !== cell && cell.r === c.r || cell.c === c.c);
-		cells.forEach(c => {
-			const ind = c.variants.findIndex(v => v === cell.result);
-			if (ind !== -1) c.variants[ind] = 0;
-		});
+	getAdjacentCells(cell) {
+		return this.cells.filter(c => c !== cell && cell.r === c.r || cell.c === c.c);
 	}
 
-	checkVariant(cell) {
-		const rowCells = this.cells.filter(c => c !== cell && cell.r === c.r).map(c => c.variants);
-		const colCells =  this.cells.filter(c => c !== cell && cell.c === c.c).map(c => c.variants);
-		
-		let result = getInd(rowCells);
-		if (result) return result;
-		result = getInd(colCells);
-		if (result) return result;
-		
-		function getInd(arrs) {
-			let result = [...arrs[0]];
-			arrs.shift();
-			for (let arr of arrs) {
-				for (let i = 0; i < arr.length; i += 1) {
-					result[i] += arr[i];
-				}
-			}
-			for (let i = 0; i < result.length; i += 1) {
-				if (result[i] === 0) {
-					return cell.variants[i]; 
-				};
-			}
-			return 0;
+	getLineCells(cell, line) {
+		return this.cells.filter(c => c !== cell && cell[line] === c[line]); 
+	}
+
+	excludeCellPossibleValue(cell) {
+		const adjacentCells = this.getAdjacentCells(cell);
+		for (let c of adjacentCells) {
+			const ind = c.possibleValues.findIndex(v => v === cell.result);
+			if (ind !== -1) c.possibleValues[ind] = 0;
 		}
+	}
+
+	getOnlyPossibleValue(cell) {
+		const rowValues = this.getLineCells(cell, 'r').map(c => c.possibleValues);
+		const colValues = this.getLineCells(cell, 'c').map(c => c.possibleValues);
+		const rowInd = sumArraysValues(rowValues).findIndex(v => v === 0);
+		const colInd = sumArraysValues(colValues).findIndex(v => v === 0);
+		if (rowInd !== -1) return cell.possibleValues[rowInd];
+		if (colInd !== -1) return cell.possibleValues[colInd];
 	}
 
 	solve() {
 		while (this.cells.length) {
 			for (let i = 0; i < this.cells.length; i += 1) {
 				let cell = this.cells[i];
-
 				const { rowKey, colKey, r, c } = cell;
-				const rowVariants = this.lookupTable.findVariants(rowKey, c, this.result.getRow(r));
-				const colVariants = this.lookupTable.findVariants(colKey, r, this.result.getCol(c));
-				cell.calc(rowVariants);
-				cell.calc(colVariants);
+				
+				const rowValues = this.lookupTable.findPossibleValues(rowKey, c, this.result.getRow(r));
+				const colValues = this.lookupTable.findPossibleValues(colKey, r, this.result.getCol(c));
+				cell.calc(rowValues);
+				cell.calc(colValues);
 
 				if (cell.result) {
 					this.result.setValue(r, c, cell.result);
 					this.cells.splice(i, 1);
-					this.excludeVariant(cell);
+					this.excludeCellPossibleValue(cell);
+
 				} else {
-	
-					const result = this.checkVariant(cell);
+					const result = this.getOnlyPossibleValue(cell);
 					if (result) {
 						cell.result = result;
 						this.result.setValue(r, c, cell.result);
 						this.cells.splice(i, 1);
-						this.excludeVariant(cell);
+						this.excludeCellPossibleValue(cell);
 					}
 				}
 			}
@@ -107,34 +91,5 @@ console.time('start');
 const clues = [0, 0, 1, 2, 0, 2, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0];
 const puzzle = new Puzzle(4, clues);
 puzzle.solve();
-puzzle.result.table.forEach(line => console.log(line));
 console.timeEnd('start');
-
-// const table = new LookupTable(4);
-// const variants = table.findVariants('00', 2, [0, 0, 0, 0]);
-// console.log(variants);
-
-// const result = new PuzzleResult(1, 4);
-// result.setValue(1, 1, 4);
-// console.log(result);
-
-// const cell = new PuzzleCell(0, 0, 0, 0, 0, 0, [1, 2, 3, 4]);
-// cell.exclude([3, 4]);
-// console.log(cell);
-
-// function calc(arrs) {
-// 	let result = arrs[0];
-// 	arrs.shift();
-// 	for (let arr of arrs) {
-// 		for (let i = 0; i < arr.length; i += 1) {
-// 			result[i] += arr[i];
-// 		}
-// 	}
-	
-// 	for (let i = 0; i < result.length; i += 1) {
-// 		if (result[i] === 0) return i;
-// 	}
-// 	return -1;
-// }
-
-// console.log(calc([[1, 1, 2, 0], [0, 0, 2, 0], [0, 0, 2, 0]]));
+puzzle.result.table.forEach(line => console.log(line));
