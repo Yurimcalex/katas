1dc4f0e6ef3b52049b7357789f3789b6e0880321
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-29 11:44:56 +0200

    Test: solve 1 puzzle 4 size 1 time

--- src/towers/index.js
+++ src/towers/index.js
@@ -1,6 +1,20 @@
 import Puzzle from './puzzle.js';
 
 
+function test_size4_number1_times1() {
+	const clues = [0, 0, 1, 2, 0, 2, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0];
+	const puzzle = new Puzzle(4, clues);
+	puzzle.solve();
+	puzzle.result.table.forEach(line => console.log(line));
+}
+
+console.time('start');
+test_size4_number1_times1();
+console.timeEnd('start');
+
+
+
+
 const data = [
 	[ 2, 2, 1, 3, 2, 2, 3, 1, 1, 2, 2, 3, 3, 2, 1, 3 ],
 	[ 1, 2, 4, 2, 2, 1, 3, 2, 3, 1, 2, 3, 3, 2, 2, 1 ],
@@ -16,14 +30,14 @@ const data = [
 ];
 
 
-console.time('start');
-//for (let i = 0; i < 1000; i += 1) {
-	const clues = [0, 0, 1, 2, 0, 2, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0];
-	const puzzle = new Puzzle(4, clues);
-	puzzle.solve();
-//}
-console.timeEnd('start');
-puzzle.result.table.forEach(line => console.log(line));
+// console.time('start');
+// for (let i = 0; i < 1000; i += 1) {
+// 	const clues = [0, 0, 1, 2, 0, 2, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0];
+// 	const puzzle = new Puzzle(4, clues);
+// 	puzzle.solve();
+// }
+// console.timeEnd('start');
+// puzzle.result.table.forEach(line => console.log(line));
 
 // console.time('data');
 // for (let d of data) {
