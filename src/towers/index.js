9c0b07c1f39b83e9a05051c463fc98781df639b8
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-29 13:14:22 +0200

    Test: solve 11 puzzle 4 size 1 time

--- src/towers/index.js
+++ src/towers/index.js
@@ -3,7 +3,8 @@ import Puzzle from './puzzle.js';
 
 console.time('start');
 //test_size4_number1_times1();
-test_size4_number1_times1000();
+//test_size4_number1_times1000();
+test_size4_number11_times1();
 console.timeEnd('start');
 
 
@@ -24,42 +25,34 @@ function test_size4_number1_times1000() {
 }
 
 
+function test_size4_number11_times1() {
+	const data = [
+		[ 2, 2, 1, 3, 2, 2, 3, 1, 1, 2, 2, 3, 3, 2, 1, 3 ],
+		[ 1, 2, 4, 2, 2, 1, 3, 2, 3, 1, 2, 3, 3, 2, 2, 1 ],
+		[ 3, 2, 2, 1, 1, 2, 4, 2, 2, 1, 3, 2, 3, 1, 2, 3 ],
+		[ 0, 0, 1, 2, 0, 2, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0 ],
+		[ 3, 2, 2, 1, 1, 2, 4, 2, 2, 1, 3, 2, 3, 1, 2, 3 ],
+		[ 2, 1, 3, 2, 3, 1, 2, 3, 3, 2, 2, 1, 1, 2, 4, 2 ],
+		[ 2, 2, 3, 1, 1, 2, 2, 3, 3, 2, 1, 3, 2, 2, 1, 3 ],
+		[ 3, 1, 2, 3, 3, 2, 2, 1, 1, 2, 4, 2, 2, 1, 3, 2 ],
+		[ 1, 2, 2, 3, 3, 2, 1, 3, 2, 2, 1, 3, 2, 2, 3, 1 ],
+		[ 3, 1, 2, 3, 3, 2, 2, 1, 1, 2, 4, 2, 2, 1, 3, 2 ],
+		[ 0, 2, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0, 0, 0, 1, 2 ]
+	];
+	for (let d of data) {
+		const puzzle = new Puzzle(4, d);
+		puzzle.solve();
+		puzzle.result.table.forEach(line => console.log(line));
+		console.log(JSON.stringify(d));
+		console.log('-------------------------------------------------');
+	}
+}
 
 
 
-const data = [
-	[ 2, 2, 1, 3, 2, 2, 3, 1, 1, 2, 2, 3, 3, 2, 1, 3 ],
-	[ 1, 2, 4, 2, 2, 1, 3, 2, 3, 1, 2, 3, 3, 2, 2, 1 ],
-	[ 3, 2, 2, 1, 1, 2, 4, 2, 2, 1, 3, 2, 3, 1, 2, 3 ],
-	[ 0, 0, 1, 2, 0, 2, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0 ],
-	[ 3, 2, 2, 1, 1, 2, 4, 2, 2, 1, 3, 2, 3, 1, 2, 3 ],
-	[ 2, 1, 3, 2, 3, 1, 2, 3, 3, 2, 2, 1, 1, 2, 4, 2 ],
-	[ 2, 2, 3, 1, 1, 2, 2, 3, 3, 2, 1, 3, 2, 2, 1, 3 ],
-	[ 3, 1, 2, 3, 3, 2, 2, 1, 1, 2, 4, 2, 2, 1, 3, 2 ],
-	[ 1, 2, 2, 3, 3, 2, 1, 3, 2, 2, 1, 3, 2, 2, 3, 1 ],
-	[ 3, 1, 2, 3, 3, 2, 2, 1, 1, 2, 4, 2, 2, 1, 3, 2 ],
-	[ 0, 2, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0, 0, 0, 1, 2 ]
-];
 
 
-// console.time('start');
-// for (let i = 0; i < 1000; i += 1) {
-// 	const clues = [0, 0, 1, 2, 0, 2, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0];
-// 	const puzzle = new Puzzle(4, clues);
-// 	puzzle.solve();
-// }
-// console.timeEnd('start');
-// puzzle.result.table.forEach(line => console.log(line));
 
-// console.time('data');
-// for (let d of data) {
-// 	const puzzle = new Puzzle(4, d);
-// 	puzzle.solve();
-// 	puzzle.result.table.forEach(line => console.log(line));
-// 	console.log(JSON.stringify(d));
-// 	console.log('-------------------------------------------------');
-// }
-// console.timeEnd('data');
 
 // const d = [0,2,0,0,0,3,0,0,0,1,0,0,0,0,1,2];
 // const puzzle = new Puzzle(4, d);
